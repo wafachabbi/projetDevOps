09e0811233f0ee4cd2d5e7c8b11949625258eb5b
Wafacg <wafachabbi21@gmail.com>
2025-12-11 18:41:59 +0100

    updates

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,8 +2,10 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
